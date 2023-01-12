715d2b4a601194099fae7430f749eb03949a2d63
LucasMarMed <lucas2marques@hotmail.com>
2023-01-12 15:42:34 +0000

    Projeto Inicial
    Criando projeto para estudo de arrays e condicionais

---
+++ bytebank_Arrays/bytebank_ATENDIMENTO/Program.cs
@@ -0,0 +1,29 @@
+﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
+Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+
+TestaArrayInt();
+
+void TestaArrayInt()
+{
+    int[] idades = new int[5];
+    idades[0] = 30;
+    idades[1] = 40;
+    idades[2] = 17;
+    idades[3] = 21;
+    idades[4] = 18;
+
+    Console.WriteLine($"Tamanho do Array {idades.Length}");
+
+    int acumulador = 0;
+    for (int i = 0; i < idades.Length; i++)
+    {
+        int idade = idades[i];
+        Console.WriteLine($"índice [{i}] = {idade}");
+        acumulador += idade;
+    }
+    int media = acumulador / idades.Length;
+    Console.WriteLine($"Média de idades = {media}");
+}
+
+//new ByteBankAtendimento().AtendimentoCliente();
+
