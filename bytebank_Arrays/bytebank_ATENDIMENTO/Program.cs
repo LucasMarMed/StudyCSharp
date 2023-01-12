f7a9895a25afb34a17a66fa237e089ef6c2625ab
LucasMarMed <lucas2marques@hotmail.com>
2023-01-12 17:37:40 +0000

    Aula 01
    Entendendo Arrays e como percorre-los

--- bytebank_Arrays/bytebank_ATENDIMENTO/Program.cs
+++ bytebank_Arrays/bytebank_ATENDIMENTO/Program.cs
@@ -1,14 +1,14 @@
-﻿using bytebank_ATENDIMENTO.bytebank.Atendimento;
-Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
+﻿Console.WriteLine("Boas Vindas ao ByteBank, Atendimento.");
 
 TestaArrayInt();
+TestaBuscarPalavra();
 
 void TestaArrayInt()
 {
     int[] idades = new int[5];
     idades[0] = 30;
-    idades[1] = 40;
-    idades[2] = 17;
+    idades[1] = 42;
+    idades[2] = 27;
     idades[3] = 21;
     idades[4] = 18;
 
@@ -24,6 +24,35 @@ void TestaArrayInt()
     int media = acumulador / idades.Length;
     Console.WriteLine($"Média de idades = {media}");
 }
+
+void TestaBuscarPalavra()
+{
+    string[] arrayDePalavras = new string[5];
+
+    for (int i = 0; i < arrayDePalavras.Length; i++)
+    {
+        Console.Write($"Digite {i + 1}ª Palavra: ");
+        arrayDePalavras[i] = Console.ReadLine();
+    }
+
+    Console.Write("Digite palavra a ser encontrada: ");
+    var busca = Console.ReadLine();
+
+    foreach (string palavra in arrayDePalavras)
+    {
+        if (palavra.Equals(busca))
+        {
+            Console.WriteLine($"Palavra encontrada = {busca}.");
+            break;
+        }
+        //else
+        //{
+        //    Console.WriteLine($"Palavra não encontrada.");
+        //}
+    }
+}
+
+
 
 //new ByteBankAtendimento().AtendimentoCliente();
 
