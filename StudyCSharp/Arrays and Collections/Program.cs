407e67b9166e0ff27d58ad6b1ce78fd2a6c943cb
LucasMarMed <lucas2marques@hotmail.com>
2023-01-19 00:10:32 +0000

    Where or SQL
    2 alternativas de implementar uma busca em uma coleção

---
+++ StudyCSharp/Arrays and Collections/Program.cs
@@ -0,0 +1,21 @@
+﻿// Esther e Cleber estão fazendo pair programming,para definir uma consulta LINQ a uma coleção de dados de Pessoas.
+// A fonte de dados que eles estão usando é um List<Pessoa> em memória.
+// Opções que retornam uma Pessoa com base no email informado (que é único por pessoa).
+
+#1° Alternativa 
+            
+Pessoa? ConsultaCliente(List<Pessoa> _pessoas, string _email)
+{
+    return _pessoas.Where(x => x.Email.Equals(_email)).FirstOrDefault();
+}
+
+
+#2° Alternativa (SQL)
+
+Pessoa? ConsultaCliente(List<Pessoa> _pessoas, string _email)
+{
+    return (from pessoa in _pessoas
+            where pessoa.Email == _email
+            select pessoa).FirstOrDefault();
+
+}
